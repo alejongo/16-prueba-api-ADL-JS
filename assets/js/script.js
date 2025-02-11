8f5d5be866b227a7a65ec02a39d2cfcd3bce57c7
Alejandro Gómez Montoya <alejongo@Alejandros-MacBook-Pro.local>
2025-02-11 06:42:55 -0500

    :zap: ADD GRAPH: Added Graph with historic money value data

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,13 @@ const textTotal = document.getElementById("textTotal");
 const convertedCurrencyLabel = document.getElementById(
   "convertedCurrencyLabel"
 );
+let chart = document.getElementById("chart");
 
 const getMoneyValue = async (currencyType = "dolar") => {
   const res = await fetch(`https://mindicador.cl/api/${currencyType}`);
   const data = await res.json();
 
-  console.log(data);
+  //console.log(data);
   return data;
 };
 
@@ -27,8 +28,6 @@ const moneyConvertion = async (quantity = 1) => {
 
   // Show Total converted
   totalConvertedMoney.setAttribute("value", formatedValue);
-
-  console.log(convertedMoneyValue);
 };
 
 // Get realtime input value
@@ -42,10 +41,68 @@ inputCurrency.addEventListener("input", (e) => {
 currencySelector.addEventListener("change", () => {
   getMoneyValue(currencySelector.value);
   moneyConvertion(inputCurrency.value);
+  //renderGraph();
   // Get Selected option Index
   const selectedIndex =
     currencySelector.options[currencySelector.selectedIndex];
   convertedCurrencyLabel.innerHTML = `${selectedIndex.text} <span class="font-bold ml-1">$</span>`;
 });
 
+//TODO
+
+// Graph Money Historic data
+const getMoneyGraphData = async (shortedData) => {
+  //const moneyData = await getMoneyValue(currencySelector.value);
+
+  const graphType = "line";
+  const title = "Money Value";
+  const lineColor = "red";
+
+  const shortedArray = shortedData.serie.splice(10);
+  console.log(shortedArray);
+  // const sorted = shortedData.serie.sort((a, b) => new Date(a) - new Date(b));
+  // console.log(sorted);
+
+  // Variables grafica
+  const labels = shortedData.serie.map((serie) => {
+    return serie.fecha.slice(0, 10);
+  });
+
+  labels.sort((a, b) => new Date(b) - new Date());
+  console.log(labels);
+
+  const data = shortedData.serie.map((serie) => {
+    return serie.valor;
+  });
+
+  data.sort((a, b) => b - a);
+
+  // Configuracion de la grafica
+  const config = {
+    type: graphType,
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: title,
+          backgroundColor: lineColor,
+          data: data,
+        },
+      ],
+    },
+  };
+
+  return config;
+};
+
+// Render Graph
+const renderGraph = async () => {
+  const data = await getMoneyValue();
+  const config = await getMoneyGraphData(data);
+
+  chart.style.backgroundColor = "white";
+  new Chart(chart, config);
+};
+
 moneyConvertion();
+renderGraph();
